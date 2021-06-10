57a50d1dc152080c3a75a2324546cd79869f8684
Erick Enriquez <erick.enriquez10.95@gmail.com>
2021-06-10 14:47:52 -0700

    clean the header from extra commented content

--- hogwarts-school-app/components/header.js
+++ hogwarts-school-app/components/header.js
@@ -2,30 +2,27 @@ import React from 'react'
 import Link from 'next/link'
 import styles from '../styles/component-styles/header.module.scss'
 import NavBar from '../components/navBar'
-//import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-//import { faTwitter } from '@fortawesome/free-brands-svg-icons'
-//import { faFacebook } from '@fortawesome/free-brands-svg-icons'
-//import { faInstagram } from '@fortawesome/free-brands-svg-icons'
-//import { config } from '@fortawesome/fontawesome-svg-core';
-//config.autoAddCss = false; /* eslint-disable import/first */
+import Image from 'next/image'
+
 
 
 class Header extends React.Component {
 	render () {
-		//const icons = (
-		//<>
-		//<FontAwesomeIcon icon={faTwitter} />
-		//<FontAwesomeIcon icon={faFacebook} />
-		//<FontAwesomeIcon icon={faInstagram} />
-		//</>
-		//)
 		return (
 			<>
 				<section className={`${styles.row} ${styles.container}`}>
-					<h6 className={styles.item_1}>Follow us on twitter, fb, insta
-
-				</h6>
-					<h3 className={styles.item_2}> Hogwarts School for Witchcraft and Wizardry</h3>
+					<h6 className={styles.item_1}>
+						Follow us on twitter, fb, insta
+					</h6>
+					<div className={styles.item_2}>
+						<Image
+							src='/images/school-crest.png'
+							alt='school-crest'
+							width='90'
+							height='90'
+						/>
+						<h3>Hogwarts School for Witchcraft and Wizardry</h3>
+					</div>
 					<div className={styles.item_3}></div>
 					<Link href="/contact-us">
 						<a className={`${styles.button} ${styles.item_4}`}>
