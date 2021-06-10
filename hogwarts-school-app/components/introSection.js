235eba26d7fadbdeddfd06a71b8bba9e946b9753
Erick Enriquez <erick.enriquez10.95@gmail.com>
2021-06-10 10:16:13 -0700

    [fix weird issues with background hero image and with the header hiding at times]

---
+++ hogwarts-school-app/components/introSection.js
@@ -0,0 +1,13 @@
+import styles from '../styles/component-styles/introsection.module.scss'
+export default function IntroSection () {
+	return (
+		<div className={styles.introSection}>
+			<h3>
+				For over 1000 years we have taken in the magically gifted from throughout the
+			</h3>
+			<h3>
+				British Isles and turned them into the Promising Wizards of the Future
+			</h3>
+		</div>
+	)
+}
