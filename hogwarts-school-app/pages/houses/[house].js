e031491f2a3a69480de2d2d6ccdfb14ea896eab6
Erick Enriquez <erick.enriquez10.95@gmail.com>
2021-06-11 15:34:44 -0700

    add state and styles to componet

--- hogwarts-school-app/pages/houses/[house].js
+++ hogwarts-school-app/pages/houses/[house].js
@@ -1,14 +1,41 @@
 import { useRouter } from 'next/router'
 import Layout from '../../components/Layout'
+import Image from 'next/image'
+import styles from '../../styles/Home.module.scss'
+import data from '../../../data/housesInfo'
+import { useEffect, useState } from 'react'
+
 const House = () => {
 	const router = useRouter()
 	const { house } = router.query
-	
+
+	const [houseActive, setHouse] = useState(false)
+
+	const getHouseInfo = async () => {
+		//need to wait for data to be received from data object in data folder about houses
+		const selectedHouse = await data.houses[house]
+		setHouse(selectedHouse)
+	}
+	//get house info after page load, since not doing this causes houseInfo to return undefined and break component
+	useEffect(() => {
+		getHouseInfo()
+	})
+
 	return (
 		<Layout>
-			<p>House IS: {house}</p>
+			<main className={styles.container}>
+				{houseActive ? (
+					<Image
+						src={houseActive.logo}
+						alt={`${house} logo`}
+						width="200"
+						height="200"
+					/>) :
+					null
+				}
+				<p>motto {houseActive.motto}</p>
+			</main>
 		</Layout>
-
 	)
 }
 
