34ba5670a9600c5f5b8ff5769dc345da4c731d7c
Erick Enriquez <erick.enriquez10.95@gmail.com>
2021-06-11 15:34:31 -0700

    create basic object that can be used for "backend"

---
+++ data/housesInfo.js
@@ -0,0 +1,21 @@
+const data = {
+	"houses": {
+		"gryffindor": {
+			logo: "/images/gryffindor_lion.png",
+			motto: "Their daring, nerve and chivalry set Gryffindors apart"
+		},
+		"hufflepuff": {
+			logo: "",
+			motto: "Those patient Hufflepuffs are true, and unafraid of toil."
+		},
+		"ravenclaw": {
+			logo: "",
+			motto: "Wit beyond measure is man's greatest treasure"
+		},
+		"slytherin": {
+			logo: "",
+			motto: "Slytherin will help you on your way to greatness"
+		}
+	}
+}
+export default data
