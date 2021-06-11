f8a02e41ff7721e8561de1d4b5c016ad205e7c4d
Erick Enriquez <erick.enriquez10.95@gmail.com>
2021-06-11 16:36:54 -0700

    added traits of each house

--- data/housesInfo.js
+++ data/housesInfo.js
@@ -3,22 +3,62 @@ const data = {
 		"gryffindor": {
 			title: 'Gryffindor',
 			logo: "/images/gryffindor_lion.png",
-			motto: "Their daring, nerve and chivalry set Gryffindors apart"
+			motto: "Their daring, nerve and chivalry set Gryffindors apart",
+			traits: [
+				"Chivalry",
+				"Brave",
+				"Daring",
+				"Nerve",
+				"Courage",
+				"Determination",
+				"Adventurous"
+			]
 		},
 		"hufflepuff": {
 			title: 'Hufflepuff',
 			logo: "/images/hufflepuff_badger.png",
-			motto: "Those patient Hufflepuffs are true, and unafraid of toil."
+			motto: "Those patient Hufflepuffs are true, and unafraid of toil.",
+			traits: [
+				'Diligence',
+				'Loyalty',
+				'Kindness',
+				'Tolerance',
+				'Dedication',
+				'Fairness',
+				'Patience',
+				'Modesty',
+			]
 		},
 		"ravenclaw": {
 			title: 'RavenClaw',
 			logo: "/images/ravenclaw_eagle.png",
-			motto: "Wit beyond measure is man's greatest treasure"
+			motto: "Wit beyond measure is man's greatest treasure",
+			traits: [
+				'Intelligence',
+				'Wit',
+				'Wisdom',
+				'Creativity',
+				'Originality',
+				'Individuality',
+				'Acceptance',
+				'Sharpness'
+			]
 		},
 		"slytherin": {
 			title: 'Slytherin',
 			logo: "/images/slytherin_snake.png",
-			motto: "Slytherin will help you on your way to greatness"
+			motto: "Slytherin will help you on your way to greatness",
+			traits: [
+				'Cunning',
+				'Ambition',
+				'Resourcefulness',
+				'Fraternity',
+				'Pride',
+				'Determination',
+				'Leadership',
+				'Self - Preservation',
+				'Lineage'
+			]
 		}
 	}
 }
