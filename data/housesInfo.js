d57d316b1186b3c13eaf4e88d7881ee7c0e35378
Erick <erick.enriquez10.95@gmail.com>
2021-06-12 11:04:54 -0700

    added founder info

--- data/housesInfo.js
+++ data/housesInfo.js
@@ -12,7 +12,16 @@ const data = {
 				"Courage",
 				"Determination",
 				"Adventurous"
-			]
+			],
+			founder: {
+				name: 'Godric Gryffindor',
+				image: '/images/founders/godric_gryffindor.jpg',
+				bio: `Said to be one of "the four most brilliant witches and wizards of the time, Godric Gryffindor founded Hogwarts School of Witchcraft and Wizardry in his adulthood, with friends Salazar Slytherin, Helga Hufflepuff, and Rowena Ravenclaw, though he later severed ties with Slytherin. He even appears to have carefully selected members of his own House.
+
+				Gryffindor is said to have praised courage, determination and strength of heart above all other qualities. Indeed, he selected students for his house based upon their daring and bravery, according to the Sorting Hat. He also was the most in favour of allowing Muggle-borns into the school.
+
+				The founders, however, were unsure of how they would Sort the students when they were dead. To solve this, Gryffindor took off his hat and all four of them enchanted it so that it could sort the students long after their deaths. It was, from then on, known as the Sorting Hat.`
+			}
 		},
 		"hufflepuff": {
 			title: 'Hufflepuff',
@@ -27,7 +36,14 @@ const data = {
 				'Fairness',
 				'Patience',
 				'Modesty',
-			]
+			],
+			founder: {
+				name: 'Helga Hufflepuff',
+				image: '/images/founders/helga_hufflepuff.jpg',
+				bio: `United by the common goal of creating the world's best magic school[8], Helga Hufflepuff, and her good friends Rowena Ravenclaw, Godric Gryffindor, and Salazar Slytherin built Hogwarts Castle together and established Hogwarts School of Witchcraft and Wizardry sometime around 993. She brought people from different backgrounds together to help in the building of the school.
+
+				While the other founders took students based on either ambition, bravery, or intelligence, Helga took the loyal, hard-working, patient and tolerant and treated them all equally.[10] She had a gift for Food-related charms, and her recipes are still used as the basis for many Hogwarts feasts. She also arranged for the house-elf contingent to work in the kitchens, giving them somewhere safe to work, where they would not be mistreated or abused`
+			}
 		},
 		"ravenclaw": {
 			title: 'RavenClaw',
@@ -42,7 +58,14 @@ const data = {
 				'Individuality',
 				'Acceptance',
 				'Sharpness'
-			]
+			],
+			founder: {
+				name: 'Rowena Ravenclaw',
+				image: '/images/founders/rowena_ravenclaw.jpg',
+				bio: `United by the common goal of creating the world's best magic school, Rowena Ravenclaw and her good friends Helga Hufflepuff, Godric Gryffindor, and Salazar Slytherin built Hogwarts Castle together and established Hogwarts School of Witchcraft and Wizardry sometime around 993. It is a popular historical theory that the location and name of Hogwarts were both chosen by Rowena, who dreamt that a warty hog was leading her to a cliff by the lake. She also came up with the ever-changing floor plan.
+
+				Rowena selected her students according to intelligence and wisdom. Ravenclaw House therefore values a sharp mind, wisdom, creativity, and cleverness in its members. At some point, Rowena bore a daughter, Helena, who was taught by Rowena herself at Hogwarts.`
+			}
 		},
 		"slytherin": {
 			title: 'Slytherin',
@@ -58,7 +81,14 @@ const data = {
 				'Leadership',
 				'Self - Preservation',
 				'Lineage'
-			]
+			],
+			founder: {
+				name: "Salazar Slytherin",
+				image: '/images/founders/salazar_slytherin.jpg',
+				bio: `Around 993 A.D., Salazar Slytherin founded Hogwarts School of Witchcraft and Wizardry in his adulthood, with friends Godric Gryffindor, Helga Hufflepuff and Rowena Ravenclaw, though he later severed ties with Gryffindor. He even appeared to have carefully selected members of his own House.
+
+				The qualities which Slytherin prized in his "handpicked students" included resourcefulness, determination, and a certain disregard for the rules, along with the ability to speak Parseltongue. He also selected his students according to their cunning, ambition, and blood purity. His House within Hogwarts was symbolised by a serpent, with House colours of green and silver.`
+			}
 		}
 	}
 }
